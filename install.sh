87c1a464dfe30ef07ba93569057d44825bf06da0
emwedk <emwedk@gmail.com>
2025-06-05 17:42:49 +0200

    init

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Install Ansible if not already installed
+if ansible --version >/dev/null 2>&1; then
+    echo "Installing Emhome with Ansible ..."
+else
+    echo "Installing Ansible"
+    sudo apk add --no-interactive --no-cache ansible
+    if [ $? -ne 0 ]; then
+        echo "Failed to install Ansible. Exiting."
+        exit 1
+    fi
+fi
+
+# Run the playbook
+ansible-playbook -i inventory playbook.yml "$@"
